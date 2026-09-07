7902fa0dfec1d1c5e5850788a0573b943793d0fa
redclover1115 <redclover1115@gmail.com>
2026-09-07 14:01:50 +0900

    Update barcode_app.py

--- barcode_app.py
+++ barcode_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # 💡 吉本さんのGoogleウェブアプリのURL
-GAS_URL = "https://google.com"
+GAS_URL = "https://script.google.com/macros/s/AKfycbzqCJKbh31A1MD19mhbLyAhQa2LxN34zs2XxREaCe64GI-1uthsF7qzn89fh36J0FH1/exec" 
 
 # --- 🔒 パスワード認証機能 ---
 def check_password():
